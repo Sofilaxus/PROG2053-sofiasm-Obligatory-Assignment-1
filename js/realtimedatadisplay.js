f993365afbb3f6ab098385072cd482e9eb5b627c
Sofia Serine <sofiasm@stud.ntnu.no>
2024-09-30 11:42:21 +0200

    did some more styling and added oslo to the weather

--- js/realtimedatadisplay.js
+++ js/realtimedatadisplay.js
@@ -4,7 +4,8 @@ const locations = [
     { name: "New York", lat: 40.7128, lon: -74.0060 },
     { name: "London", lat: 51.5074, lon: -0.1278 },
     { name: "Paris", lat: 48.8566, lon: 2.3522 },
-    { name: "Sydney", lat: -33.8688, lon: 151.2093 }
+    { name: "Sydney", lat: -33.8688, lon: 151.2093 },
+    { name: "Oslo", lat: 59.9115, lon: 10.7579 }
 ];
 
 const weatherData = {};
